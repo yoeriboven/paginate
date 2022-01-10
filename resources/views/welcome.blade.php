5d878a4d0bef64fcf5639c18efaf6cda8ffc5750
Yoeri Boven <yoeriboven@msn.com>
2022-01-10 18:26:29 +0100

    Added Tailwind CSS

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
+
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
         @foreach ($users as $user)
